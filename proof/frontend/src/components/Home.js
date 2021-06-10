aa41045e6bf07aa2dfdfa8c57b54d4ec75601054
Nr Aziz <nr.aziz@gmail.com>
2021-06-10 15:08:46 +0500

    Changes

--- proof/frontend/src/components/Home.js
+++ proof/frontend/src/components/Home.js
@@ -26,55 +26,124 @@ function Home(props) {
 
       <section className="about-program py-5">
         <div className="container">
-              <div className="about-program__header ">
+          <div class="row">
+              <div class="col-md-6">
+                <div className="about-program__header ">
+                </div>              
+              </div>
+              <div class="col-md-6">
                 <h1>ABOUT THE PROGRAM</h1>
-              </div>              
-              <h2 className="subtitle">Learn by DOING!</h2>
-              <p className="text-grey font-18 py-3">
-              Too many programs are horizontal in nature. They just scratch the surface of wealth building, but the “Proof of Success” program is going to change the game for online learning and content. Filled with breakthroughs and advanced real-life business strategies that will work for you instantly, it’s strategies that worked yesterday, today and will work for you tomorrow.
-              </p>
+                <p className="text-grey font-18 py-3">
+                Too many programs are horizontal in nature. They just scratch the surface of wealth building, but the “Proof of Success” program is going to change the game for online learning and content. Filled with breakthroughs and advanced real-life business strategies that will work for you instantly, it’s strategies that worked yesterday, today and will work for you tomorrow.
+                </p>
+              </div>
+          </div>
         </div>
       </section>
 
-      <section className="the-experts py-5">
-        <div className="container">
+      <section class="the-experts py-5">
+        <div class="container">
+          <div class="row">
             <h1>THE EXPERTS</h1>
-            <p className="font-18 mb-5">
+            <p class="text-grey font-18">
               The hosts of your program and who you'll be learning from...
             </p>
-
-            <div className="expert-boxes">
-              <div className="expert">
-                <img src="/media/images/frontend/jtfoxx.jpg" alt="JT Foxx"/>
-                <h6>JT Foxx</h6>
-                <i>World’s #1 Wealth & Business Coach and entrepreneur of many companies and brands</i>
-              </div>
-              <div className="expert">
-                <img src="/media/images/frontend/george-ross.jpeg" alt="George Ross"/>
-                <h6>George Ross</h6>
-                <i>JT Foxx’s Coach & Celebrity Apprentice Judge</i>
-              </div>
-              <div className="expert">
-                <img src="/media/images/frontend/hugh-hilton.jpg" alt="Hugh Hilton"/>
-                <h6>Hugh Hilton</h6>
-                <i>JT Foxx’s Coach & Property & Business Mogul</i>
-              </div>
-            </div>
-            <div className="expert-boxes">
-              <div className="expert">
-                <img src="/media/images/frontend/mike-slade.jpg" alt="Mike Slade"/>
-                <h6>Mike Slade</h6>
-                <i>JT Foxx’s Coach & Venture Capitalist who worked for Steve Job and Bill Gates. Created ESPN.com and sold to ABC (now Disney)</i>
-              </div>
-              <div className="expert">
-                <img src="/media/images/frontend/tycoonx.jpg" alt="Tycoon X"/>
-                <h6>Tycoon X</h6>
-                <i>JT Foxx's Coach & 10-Figure Property Tycoon</i>
+            <div class="col-md-4 mt-3">
+              <div class="border-green border-bottom-right">
+                <img
+                  src="/media/images/frontend/jtfoxx.jpg"
+                  class="img-fluid"
+                  alt="JT Foxx"
+                />
+                <div class="about-speaker">
+                  <div class="inner">
+                    <h2>JT Foxx</h2>
+                    <p>
+                    World’s #1 Wealth & Business Coach and entrepreneur of many companies and brands
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
 
+            <div class="col-md-4 mt-3">
+              <div class="border-green border-bottom-right">
+                <img
+                  src="/media/images/frontend/george-ross.jpeg"
+                  class="img-fluid"
+                  alt="George Ross"
+                />
+                <div class="about-speaker">
+                  <div class="inner">
+                    <h2>George Ross</h2>
+                    <p>
+                    JT Foxx’s Coach & Celebrity Apprentice Judge
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 mt-3">
+              <div class="border-green border-bottom-right">
+                <img
+                  src="/media/images/frontend/hugh-hilton.jpg"
+                  class="img-fluid"
+                  alt="Hugh Hilton"
+                />
+                <div class="about-speaker">
+                  <div class="inner">
+                    <h2>Hugh Hilton</h2>
+                    <p>
+                    JT Foxx’s Coach & Property & Business Mogul
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 mt-3">
+              <div class="border-green border-bottom-right">
+                <img
+                  src="/media/images/frontend/mike-slade.jpg"
+                  class="img-fluid"
+                  alt="Tycoon X"
+                />
+                <div class="about-speaker">
+                  <div class="inner">
+                    <h2>Tycoon X</h2>
+                    <p>
+                    JT Foxx's Coach & 10-Figure Property Tycoon
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4 mt-3">
+              <div class="border-green border-bottom-right">
+                <img
+                  src="/media/images/frontend/tycoonx.jpg"
+                  class="img-fluid"
+                  alt="Mike Slade"
+                />
+                <div class="about-speaker">
+                  <div class="inner">
+                    <h2>Tycoon X</h2>
+                    <p>
+                    JT Foxx’s Coach & Venture Capitalist who worked for Steve Job and Bill Gates. Created ESPN.com and sold to ABC (now Disney)
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
         </div>
-      </section>
+      </section>      
+
+
 
       <section className="featured-modules py-5">
         <div className="container">
