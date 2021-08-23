ca5ad06e917993f6d72989eccd7ff1f5be8bbf4d
Nr Aziz <nr.aziz@gmail.com>
2021-08-23 22:36:18 +0500

    Changes

--- proof/frontend/src/components/Home.js
+++ proof/frontend/src/components/Home.js
@@ -5,8 +5,7 @@ import Box from "./Box";
 function Home(props) {
   let featuredPrograms = props.programs.map((program, i) => {
     const ids = [2, 4, 5];
-    if (ids.includes(program.id))
-      return <Box key={i} program={program} />;
+    if (ids.includes(program.id)) return <Box key={i} program={program} />;
   });
 
   return (
@@ -17,7 +16,7 @@ function Home(props) {
             <div className="col-12">
               <h1>PROOF OF SUCCESS PROGRAM</h1>
               <h2>Your Quickest Path to Getting Results!</h2>
-              <Link className="custombtn" to="/modules">
+              <Link className="custombtn" to="/categories">
                 GO TO MODULES
               </Link>
             </div>
@@ -28,16 +27,21 @@ function Home(props) {
       <section className="about-program py-5">
         <div className="container">
           <div class="row">
-              <div class="col-md-6">
-                <div className="about-program__header ">
-                </div>              
-              </div>
-              <div class="col-md-6">
-                <h1>ABOUT THE PROGRAM</h1>
-                <p className="text-grey font-18 py-3">
-                Too many programs are horizontal in nature. They just scratch the surface of wealth building, but the “Proof of Success” program is going to change the game for online learning and content. Filled with breakthroughs and advanced real-life business strategies that will work for you instantly, it’s strategies that worked yesterday, today and will work for you tomorrow.
-                </p>
-              </div>
+            <div class="col-md-6">
+              <div className="about-program__header "></div>
+            </div>
+            <div class="col-md-6">
+              <h1>ABOUT THE PROGRAM</h1>
+              <p className="text-grey font-18 py-3">
+                Too many programs are horizontal in nature. They just scratch
+                the surface of wealth building, but the “Proof of Success”
+                program is going to change the game for online learning and
+                content. Filled with breakthroughs and advanced real-life
+                business strategies that will work for you instantly, it’s
+                strategies that worked yesterday, today and will work for you
+                tomorrow.
+              </p>
+            </div>
           </div>
         </div>
       </section>
@@ -60,7 +64,8 @@ function Home(props) {
                   <div class="inner">
                     <h2>JT Foxx</h2>
                     <p>
-                    World’s #1 Wealth & Business Coach and entrepreneur of many companies and brands
+                      World’s #1 Wealth & Business Coach and entrepreneur of
+                      many companies and brands
                     </p>
                   </div>
                 </div>
@@ -77,9 +82,7 @@ function Home(props) {
                 <div class="about-speaker">
                   <div class="inner">
                     <h2>George Ross</h2>
-                    <p>
-                    JT Foxx’s Coach & Celebrity Apprentice Judge
-                    </p>
+                    <p>JT Foxx’s Coach & Celebrity Apprentice Judge</p>
                   </div>
                 </div>
               </div>
@@ -90,15 +93,13 @@ function Home(props) {
                 <img
                   src="/media/images/frontend/hugh-hilton.jpg"
                   class="img-fluid"
-                  style={{'object-position': '-50px bottom'}}
+                  style={{ "object-position": "-50px bottom" }}
                   alt="Hugh Hilton"
                 />
                 <div class="about-speaker">
                   <div class="inner">
                     <h2>Hugh Hilton</h2>
-                    <p>
-                    JT Foxx’s Coach & Property & Business Mogul
-                    </p>
+                    <p>JT Foxx’s Coach & Property & Business Mogul</p>
                   </div>
                 </div>
               </div>
@@ -115,7 +116,9 @@ function Home(props) {
                   <div class="inner">
                     <h2>Mike Slade</h2>
                     <p>
-                    JT Foxx’s Coach & Venture Capitalist who worked for Steve Job and Bill Gates. Created ESPN.com and sold to ABC (now Disney)
+                      JT Foxx’s Coach & Venture Capitalist who worked for Steve
+                      Job and Bill Gates. Created ESPN.com and sold to ABC (now
+                      Disney)
                     </p>
                   </div>
                 </div>
@@ -132,34 +135,11 @@ function Home(props) {
                 <div class="about-speaker">
                   <div class="inner">
                     <h2>Tycoon X</h2>
-                    <p>
-                    JT Foxx's Coach & 10-Figure Property Tycoon
-                    </p>
+                    <p>JT Foxx's Coach & 10-Figure Property Tycoon</p>
                   </div>
                 </div>
               </div>
             </div>
-
-
-          </div>
-        </div>
-      </section>      
-
-
-
-      <section className="featured-modules py-5">
-        <div className="container">
-          <div className="row">
-            <h1 className="col-md-8 pb-5">
-              FEATURED MODULES
-            </h1>
-            <div class="col-md-4">
-              <Link className="custombtn" to="/modules">
-                GO TO ALL MODULES >>
-              </Link>
-            </div>
-                          
-            <div className="boxes">{featuredPrograms}</div>
           </div>
         </div>
       </section>
