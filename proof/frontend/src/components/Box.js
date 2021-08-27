6e68493c54d025df485cced931ccb407542676e4
Nr Aziz <nr.aziz@gmail.com>
2021-08-27 10:07:20 +0500

    Changes

--- proof/frontend/src/components/Box.js
+++ proof/frontend/src/components/Box.js
@@ -5,7 +5,7 @@ function Box({ program }) {
   return (
     <div className="box">
       <Link to={"/program/" + program.id} style={{ textDecoration: "none" }}>
-        <img src={program.image} alt={program.title} class="img-fluid" />
+        <img src={program.image} alt={program.name} class="img-fluid" />
       </Link>
       <div className="box-content text-center">
         <Link className="btn-blue mb-2" to={"/program/" + program.id}>
